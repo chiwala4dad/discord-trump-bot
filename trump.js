17695f5b00d42a3922853fba14d5488eb7266810
chiwala4dad <chiwala4dad@gmail.com>
2020-07-29 22:40:26 -0400

    Update trump.js

--- trump.js
+++ trump.js
@@ -1,46 +1,26 @@
-const tokenFile = require("./token.js");
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
 
-const fs = require("fs");
-const _ = require("underscore");
-const moment = require("moment");
-const discord = require('discord.js');
+const client = new Discord.Client();
 
-const client = new discord.Client();
+client.on('message', message => {
+	if (message.content === '!play') {
+		if (message.channel.type !== 'text') return;
 
-// store the filenames of all the audio files for sampling later
+		const voiceChannel = message.member.voice.channel;
 
+		if (!voiceChannel) {
+			return message.reply('please join a voice channel first!');
+		}
 
-client.on("ready", () => {
-   console.log("I'm ready!");
+		voiceChannel.join().then(connection => {
+			const stream = ytdl('https://www.youtube.com/watch?v=D57Y1PruTlw', { filter: 'audioonly' });
+			const dispatcher = connection.play(stream);
+
+			dispatcher.on('finish', () => voiceChannel.leave());
+		});
+	}
 });
 
-client.on('message',  message => {
-
-   // if the sent message was "ping" then respond with "pong"
-   if (message.content === "ping") {
-       message.channel.send("pong");
-
-   // if the sent message was "!trump" then play a random trump quote from our data directory
-   } else if (message.content === "!trump") {
-
-       // make sure the user is in a voice channel
-       if (message.member.voiceChannel) {
-
-           // join the user's voice channel
-         const connection =  message.member.voiceChannel.join()
-               .then(connection => {
-                 // play the random audio file
-                   const dispatcher = connection.playFile('/data/I-like-China.wav');
-
-                   // disconnect from the voice channel when the quote is over
-                   dispatcher.on('finish', () => {
-                      message.memeber.voicehannel.leave()
-                      
-                   });
-               })
-               .catch(console.log);
-       }
-   }
-});
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
