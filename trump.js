f859096ea83a0dbd73d721c0fc24dd5024c15ddc
chiwala4dad <chiwala4dad@gmail.com>
2020-07-31 00:31:08 -0400

    Update trump.js

--- trump.js
+++ trump.js
@@ -8,7 +8,7 @@ client.on('message', message => {
 		message.reply('hemblo')
 	}
 	else if (message.content === 'cheemz commands') {
-		message.reply('hemblo welcome to Cheemz bot. \n The prefix is "!ch" and this is the sound list. \n pizza \n ay yo \n oh yo \n nani \n yes \n doing tho ')
+		message.reply('hemblo welcome to Cheemz bot. \n The prefix is "!ch" and this is the sound list. \n pizza \n ay yo \n oh yo \n nani \n yes \n doing tho \n what ')
 	}
 	else if (message.content === 'yes') {
 		message.reply('https://i.kym-cdn.com/photos/images/newsfeed/001/650/747/aaf.png ')
@@ -109,7 +109,22 @@ client.on('message', message => {
 			dispatcher.on('finish', () => voiceChannel.leave());
 		});
 	}
-	
+	else if (message.content === '!ch what') {
+		if (message.channel.type !== 'text') return;
+
+		const voiceChannel = message.member.voice.channel;
+
+		if (!voiceChannel) {
+			return message.reply('please join a voice channel first!');
+		}
+
+		voiceChannel.join().then(connection => {
+			//const stream = ytdl('https://www.youtube.com/watch?v=kLaaJ_aeoyM', { filter: 'audioonly' });
+			const dispatcher = connection.play('what.mp3');
+
+			dispatcher.on('finish', () => voiceChannel.leave());
+		});
+	}
 });
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
